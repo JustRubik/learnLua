52b8495503f19ab7ab83976453084c3e9fd4e62f
JustRubik <danghuyhieu1434@gmail.com>
2026-08-21 23:50:39 +0700

    leetcode 1

--- src/main.lua
+++ src/main.lua
@@ -1,5 +1,16 @@
-function a()
-  return 1, 2, 3
-end
+-- Leetcode #1 
 
-print(a(), 100)
+function twoSum(nums, target)
+  if type(nums) ~= "table" then
+    return nil, "Error: False type"
+  end
+  if type(target) ~= "number" then
+    return nil, "Error: False type"    
+  end
+
+  for i, I in ipairs(nums) do
+    for j=i,#nums,1 do
+      
+    end
+  end
+end
